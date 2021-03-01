d4f5a927f03643f095170cd317482dbdadb05d2c
zhjwang <695331215@qq.com>
2021-03-01 16:46:28 +0800

    伙伴算法

--- boot.c
+++ boot.c
@@ -7,6 +7,10 @@ extern void start_mmu();
 extern void test_printk(void);
 extern void umask_int(unsigned int);
 extern void enable_irq(void);
+extern void init_page_map(void);
+extern void *get_free_pages(unsigned int , int );
+extern void put_free_pages(void *, int );
+extern void printk(const char *fmt, ...);
 
 #define TIMER_BASE (0xd1000000)
 #define TCFG0 ((volatile unsigned int *)(TIMER_BASE + 0x0))
@@ -14,7 +18,7 @@ extern void enable_irq(void);
 #define TCON ((volatile unsigned int *)(TIMER_BASE + 0x8))
 #define TCONB4 ((volatile unsigned int *)(TIMER_BASE + 0x3c))
 
-void timer_init(void)
+	void timer_init(void)
 {
 	*TCFG0 |= 0x800;
 	*TCON &= (~(7 << 20));
@@ -62,7 +66,21 @@ void plat_boot(void)
 	start_mmu();
 	test_mmu();
 	test_printk();
-	timer_init();
+	//timer_init();
+
+	init_page_map();
+	char *p1, *p2, *p3, *p4;
+	p1 = (char *)get_free_pages(0, 6);
+	printk("6.1_the return address of get_free_pages %x\n", p1);
+	p2 = (char *)get_free_pages(0, 6);
+	printk("6.1_the return address of get_free_pages %x\n", p2);
+	put_free_pages(p2, 6);
+	put_free_pages(p1, 6);
+	p3 = (char *)get_free_pages(0, 7);
+	printk("6.1_the return address of get_free_pages %x\n", p3);
+	p4 = (char *)get_free_pages(0, 7);
+	printk("6.1_the return address of get_free_pages %x\n", p4);
+
 	while(1);
 }
 /*
